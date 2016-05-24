324bc994f2b55214c612ad23955a8916a7cd7207
Mohammad Selim Miah <selimcse98@gmail.com>
2016-05-25 00:33:37 +1000

    first commit

---
+++ app/src/main/java/com/selim/com/fragmentvogella/RssfeedActivity.java
@@ -0,0 +1,22 @@
+package com.selim.com.fragmentvogella;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class RssfeedActivity extends Activity implements MyListFragment.OnItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rssfeed);
+    }
+
+    @Override
+    public void onRssItemSelected(String link) {
+        DetailFragment fragment = (DetailFragment) getFragmentManager()
+                .findFragmentById(R.id.detailFragment);
+        fragment.setText(link);
+    }
+
+}
+
